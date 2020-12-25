e3137d9a138fdde57b2555ecd5c0574344f5505f
sarjaamisepehr <sarjamisepehr@gmail.com>
2020-12-24 17:02:12 -0800

    version 1

---
+++ hw-8/1.sh
@@ -0,0 +1,5 @@
+$ git config credential.helper store
+$ git push https://github.com/repo.git
+
+Username for 'https://github.com': <USERNAME>
+Password for 'https://USERNAME@github.com': <PASSWORD>
---
+++ hw-8/3.sh
@@ -0,0 +1,2 @@
+sepehr@ubuntu:~$ uptime
+ 16:54:40 up  3:10,  1 user,  load average: 0.42, 0.17, 0.14
